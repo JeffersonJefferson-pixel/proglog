727b7cccfe4bf55c9ca217c3cb9efc853c360658
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2024-08-30 23:07:27 +0700

    add index read write
    add index test

--- internal/log/index.go
+++ internal/log/index.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"io"
 	"os"
 
 	"github.com/tysonmote/gommap"
@@ -12,6 +13,7 @@ var (
 	entWidth           = offsetWidth + posWidth
 )
 
+// index entries contains the record's offset and its position in the store file
 type index struct {
 	file *os.File
 	mmap gommap.MMap
@@ -29,7 +31,7 @@ func newIndex(f *os.File, c Config) (*index, error) {
 	}
 	idx.size = uint64(fi.Size())
 	// grpw the file to max index size
-	if err = os.Truncate(f.Name(), int64(c.Segment.MaxindexBytes)); err != nil {
+	if err = os.Truncate(f.Name(), int64(c.Segment.MaxIndexBytes)); err != nil {
 		return nil, err
 	}
 	// mmap index file
@@ -54,4 +56,43 @@ func (i *index) Close() error {
 		return err
 	}
 	return i.file.Close()
+}
+
+// takes an offset and returns the record's position in the store.
+func (i *index) Read(in int64) (out uint32, pos uint64, err error) {
+	// given offset is relative to the segment's base offset
+	if i.size == 0 {
+		return 0, 0, io.EOF
+	}
+	if in == -1 {
+		out = uint32((i.size / entWidth) - 1)
+	} else {
+		out = uint32(in)
+	}
+	pos = uint64(out) * entWidth
+	if i.size < pos+entWidth {
+		return 0, 0, io.EOF
+	}
+	// offset
+	out = enc.Uint32(i.mmap[pos : pos+offsetWidth])
+	// position
+	pos = enc.Uint64(i.mmap[pos+offsetWidth : pos+entWidth])
+	return out, pos, nil
+}
+
+// append the given offest and position to the index.
+func (i *index) Write(offset uint32, pos uint64) error {
+	// check fi there is space to write entry
+	if uint64(len(i.mmap)) < i.size+entWidth {
+		return io.EOF
+	}
+	// encode offset and position and write to mmap
+	enc.PutUint32(i.mmap[i.size:i.size+offsetWidth], offset)
+	enc.PutUint64(i.mmap[i.size+offsetWidth:i.size+entWidth], pos)
+	i.size += uint64(entWidth)
+	return nil
+}
+
+func (i *index) Name() string {
+	return i.file.Name()
 }
