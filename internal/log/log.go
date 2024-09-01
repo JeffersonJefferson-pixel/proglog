c3b339118df07d414e22d71af0b2ed16100e71e9
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2024-09-01 10:21:07 +0700

    add log
    add log test

---
+++ internal/log/log.go
@@ -0,0 +1,196 @@
+package log
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path"
+	api "proglog/api/v1"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+// log manages list of segments.
+type Log struct {
+	mu            sync.RWMutex
+	Dir           string
+	Config        Config
+	activeSegment *segment
+	segments      []*segment
+}
+
+type originReader struct {
+	*store
+	offset int64
+}
+
+func NewLog(dir string, c Config) (*Log, error) {
+	if c.Segment.MaxStoreBytes == 0 {
+		c.Segment.MaxStoreBytes = 1024
+	}
+	if c.Segment.MaxIndexBytes == 0 {
+		c.Segment.MaxIndexBytes = 1024
+	}
+	l := &Log{
+		Dir:    dir,
+		Config: c,
+	}
+	return l, l.setup()
+}
+
+func (l *Log) setup() error {
+	files, err := os.ReadDir(l.Dir)
+	if err != nil {
+		return err
+	}
+	var baseOffsets []uint64
+	for _, file := range files {
+		offsetStr := strings.TrimSuffix(file.Name(), path.Ext(file.Name()))
+		offset, _ := strconv.ParseUint(offsetStr, 10, 0)
+		baseOffsets = append(baseOffsets, offset)
+	}
+	sort.Slice(baseOffsets, func(i, j int) bool {
+		return baseOffsets[i] < baseOffsets[j]
+	})
+	// load segments that already exists on disk
+	for i := 0; i < len(baseOffsets); i++ {
+		if err = l.newSegment(baseOffsets[i]); err != nil {
+			return err
+		}
+		i++
+	}
+	// if log is new, bootstrap initial segment
+	if l.segments == nil {
+		if err = l.newSegment(l.Config.Segment.InitialOffset); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// appends record to the log
+func (l *Log) Append(record *api.Record) (uint64, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	offset, err := l.activeSegment.Append(record)
+	if err != nil {
+		return 0, err
+	}
+	// if segment is at its max size, make a new active segment
+	if l.activeSegment.IsMaxed() {
+		err = l.newSegment(offset + 1)
+	}
+	return offset, err
+}
+
+// read record stored at the given offset
+func (l *Log) Read(offset uint64) (*api.Record, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	// find segment that contains the given record
+	var s *segment
+	for _, segment := range l.segments {
+		if segment.baseOffset <= offset && offset < segment.nextOffset {
+			s = segment
+			break
+		}
+	}
+	if s == nil || s.nextOffset <= offset {
+		return nil, fmt.Errorf("offset out of range: %d", offset)
+	}
+	return s.Read(offset)
+}
+
+// close log's segments
+func (l *Log) Close() error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	for _, segment := range l.segments {
+		if err := segment.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// closes the log and remove its data
+func (l *Log) Remove() error {
+	if err := l.Close(); err != nil {
+		return err
+	}
+	return os.RemoveAll(l.Dir)
+}
+
+// remove the log and create new log
+func (l *Log) Reset() error {
+	if err := l.Remove(); err != nil {
+		return err
+	}
+	return l.setup()
+}
+
+// for replication
+func (l *Log) LowestOffset() (uint64, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	return l.segments[0].baseOffset, nil
+}
+
+func (l *Log) HighestOffset() (uint64, error) {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	offset := l.segments[len(l.segments)-1].nextOffset
+	if offset == 0 {
+		return 0, nil
+	}
+	return offset - 1, nil
+}
+
+// removes all segments whose highest offset is lower than the lowest
+func (l *Log) Truncate(lowest uint64) error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	var segments []*segment
+	for _, s := range l.segments {
+		if s.nextOffset <= lowest+1 {
+			if err := s.Remove(); err != nil {
+				return err
+			}
+			continue
+		}
+		segments = append(segments, s)
+	}
+	l.segments = segments
+	return nil
+}
+
+// returns an io.Reader to read the whole log
+func (l *Log) Reader() io.Reader {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	readers := make([]io.Reader, len(l.segments))
+	for i, segment := range l.segments {
+		readers[i] = &originReader{segment.store, 0}
+	}
+	// concatenate segments' store
+	return io.MultiReader(readers...)
+}
+
+func (o *originReader) Read(p []byte) (int, error) {
+	n, err := o.ReadAt(p, o.offset)
+	o.offset += int64(n)
+	return n, err
+}
+
+// create new segment
+func (l *Log) newSegment(offset uint64) error {
+	s, err := newSegment(l.Dir, offset, l.Config)
+	if err != nil {
+		return err
+	}
+	l.segments = append(l.segments, s)
+	l.activeSegment = s
+	return nil
+}
